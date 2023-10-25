ab4f459a03954bcca116720b11e4121701239fa0
Carolina Carrilho <a.carolina.carrilho@gmail.com>
2023-10-25 16:55:37 +0100

    feat: site navgation page

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <nav
+      class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+    >
+    <RouterLink :to="{name: 'home'}">
+      <div class="flex items-center gap-3 flex-1">
+        <i class="fa-solid fa-sun text-2xl"></i>
+        <p class="text-2xl">The Local Weather</p>
+      </div>
+    </RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script setup></script>
